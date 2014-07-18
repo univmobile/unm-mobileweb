169e95c7be004647468cd4422cac8c965f166fe0
David <david.andriana@avantage-compris.com>
2014-07-18 16:35:53 +0200

    Initial import: Webapp with "Hello World!"

---
+++ unm-webmobile-app/src/main/java/fr/univmobile/webmobile/UnivMobileServlet.java
@@ -0,0 +1,50 @@
+package fr.univmobile.webmobile;
+
+import static org.apache.commons.lang3.CharEncoding.UTF_8;
+import static org.apache.commons.lang3.StringUtils.substringBeforeLast;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UnivMobileServlet extends HttpServlet {
+
+	/**
+	 * for serialization.
+	 */
+	private static final long serialVersionUID = -3778148036214513471L;
+
+	@Override
+	public void init() throws ServletException {
+
+		super.init();
+	}
+	
+	@Override
+	public void service(final HttpServletRequest request,
+			final HttpServletResponse response) throws IOException,
+			ServletException {
+
+		// 1. DISPATCH TO JSP
+
+		final String jspPath = "/WEB-INF/jsp/home.jsp";
+
+		final RequestDispatcher rd = request.getRequestDispatcher(jspPath);
+
+		request.setCharacterEncoding(UTF_8);
+		response.setContentType("text/html");
+		response.setCharacterEncoding(UTF_8);
+		// response.setHeader("Content-Language", "en");
+		response.setLocale(Locale.FRENCH);
+
+		rd.forward(request, response);
+		
+	}
+}
---
+++ unm-webmobile-app/src/main/webapp/WEB-INF/jsp/home.jsp
@@ -0,0 +1,26 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page pageEncoding="UTF-8"%> 
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="Content-Language" content="en">
+<title>UnivMobile</title>
+<link type="text/css" rel="stylesheet" href="./css/styles.css">
+</head>
+<body>
+
+<h1>UnivMobile</h1>
+
+Hello World!
+
+<div id="div-selectedUniversity">
+Aucune université sélectionnée
+</div>
+
+<div id="div-choisir">
+Choisir…
+</div>
+
+</body>
+</html>
