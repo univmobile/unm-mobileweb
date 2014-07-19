c5b29d453866498ec7863c3ee34d652cf203b266
David <david.andriana@avantage-compris.com>
2014-07-19 23:32:15 +0200

    Refactor: Use a ScenarioContext holder instead of passing many a arguments to AbstractScenarioTest constructor

--- unm-mobileweb-it/src/test/java/fr/univmobile/mobileweb/it/AllScenariosTest.java
+++ unm-mobileweb-it/src/test/java/fr/univmobile/mobileweb/it/AllScenariosTest.java
@@ -1,41 +1,24 @@
 package fr.univmobile.mobileweb.it;
 
-import java.lang.reflect.Method;
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
 import fr.univmobile.it.commons.AbstractScenariosTest;
-import fr.univmobile.it.commons.AppiumEnabledTest;
-import fr.univmobile.it.commons.AppiumEnabledTestPhasedEngine;
+import fr.univmobile.it.commons.ScenarioContext;
 
 @RunWith(Parameterized.class)
 public class AllScenariosTest extends AbstractScenariosTest {
 
-	@Parameters(name = "{1}.{2}.{3}_{0}")
+	@Parameters(name = "{0}")
 	public static Iterable<Object[]> parameters() throws Exception {
 
 		return AbstractScenariosTest.loadParametersForScenarioClasses( //
 				Scenarios001.class);
 	}
 
-	public AllScenariosTest(final String normalizedDeviceName, //
-			final String scenarioClassSimpleName, //
-			final String scenarioMethodName, //
-			final String engineSimpleName, //
-			final String deviceName, //
-			final Class<? extends AppiumEnabledTest> scenariosClass, //
-			final Method scenarioMethod, //
-			final AppiumEnabledTestPhasedEngine engine) {
+	public AllScenariosTest(final ScenarioContext context) {
 
-		super(normalizedDeviceName, //
-				scenarioClassSimpleName, //
-				scenarioMethodName, //
-				engineSimpleName, //
-				deviceName, //
-				scenariosClass, //
-				scenarioMethod, //
-				engine);
+		super(context);
 	}
 }
