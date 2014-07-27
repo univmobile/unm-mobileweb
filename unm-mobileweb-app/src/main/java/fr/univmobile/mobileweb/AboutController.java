1833eb11224646923e9e6a130c717c3ffe7afe28
David <david.andriana@avantage-compris.com>
2014-07-27 23:26:27 +0200

    Move buildInfo loading from unm-mobileweb-app to unm-webapp-commons.

--- unm-mobileweb-app/src/main/java/fr/univmobile/mobileweb/AboutController.java
+++ unm-mobileweb-app/src/main/java/fr/univmobile/mobileweb/AboutController.java
@@ -1,12 +1,9 @@
 package fr.univmobile.mobileweb;
 
-import static fr.univmobile.web.commons.DataBeans.instantiate;
-
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 
 import fr.univmobile.web.commons.AbstractController;
+import fr.univmobile.web.commons.BuildInfoUtils;
 import fr.univmobile.web.commons.Paths;
 
 @Paths({ "about", "about/" })
@@ -15,44 +12,9 @@ public class AboutController extends AbstractController {
 	@Override
 	public String action() throws IOException {
 
-		final Properties properties = new Properties();
-
-		final InputStream is = getServletContext().getResourceAsStream(
-				"META-INF/MANIFEST.MF");
-		try {
-
-			properties.load(is);
-
-		} finally {
-			is.close();
-		}
-
-		setAttribute(
-				"buildInfo",
-				instantiate(BuildInfo.class)
-						//
-						.setBuildDisplayName(
-								properties
-										.getProperty("Buildinfo-BuildDisplayName"))
-						//
-						.setBuildId(properties.getProperty("Buildinfo-BuildId")) //
-						.setGitCommitId(properties.getProperty("Buildinfo-Rev")));
+		setAttribute("buildInfo",
+				BuildInfoUtils.loadBuildInfo(getServletContext()));
 
 		return "about.jsp";
 	}
-}
-
-interface BuildInfo {
-
-	String getBuildDisplayName();
-
-	BuildInfo setBuildDisplayName(String buildDisplayName);
-
-	String getBuildId();
-
-	BuildInfo setBuildId(String buildId);
-
-	String getGitCommitId();
-
-	BuildInfo setGitCommitId(String gitCommitId);
 }
