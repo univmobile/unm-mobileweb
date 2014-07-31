254314a6dd94ed6eab3a75ae2f281443401a581c
David <david.andriana@avantage-compris.com>
2014-07-31 04:53:04 +0200

    Depend on unm-backend-client. Load RegionClient from init-params in web.xml

--- unm-mobileweb-app/src/main/java/fr/univmobile/mobileweb/AboutController.java
+++ unm-mobileweb-app/src/main/java/fr/univmobile/mobileweb/AboutController.java
@@ -1,7 +1,10 @@
 package fr.univmobile.mobileweb;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import java.io.IOException;
 
+import fr.univmobile.backend.client.RegionClient;
 import fr.univmobile.web.commons.AbstractController;
 import fr.univmobile.web.commons.BuildInfoUtils;
 import fr.univmobile.web.commons.Paths;
@@ -9,6 +12,13 @@ import fr.univmobile.web.commons.Paths;
 @Paths({ "about", "about/" })
 public class AboutController extends AbstractController {
 
+	public AboutController(final RegionClient regions) {
+		
+		this.regions = checkNotNull(regions,"regions");		
+	}
+	
+	private final RegionClient regions;
+	
 	@Override
 	public String action() throws IOException {
 
--- unm-mobileweb-app/src/main/java/fr/univmobile/mobileweb/UnivMobileServlet.java
+++ unm-mobileweb-app/src/main/java/fr/univmobile/mobileweb/UnivMobileServlet.java
@@ -1,7 +1,15 @@
 package fr.univmobile.mobileweb;
 
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 
+import fr.univmobile.backend.client.RegionClient;
+import fr.univmobile.commons.DependencyInjection;
 import fr.univmobile.web.commons.AbstractUnivMobileServlet;
 
 public class UnivMobileServlet extends AbstractUnivMobileServlet {
@@ -11,12 +19,46 @@ public class UnivMobileServlet extends AbstractUnivMobileServlet {
 	 */
 	private static final long serialVersionUID = -3778148036214513471L;
 
+	private RegionClient regions;
+
 	@Override
 	public void init() throws ServletException {
 
+		final Map<String, String> initParams = loadInitParams();
+
+		regions = new DependencyInjection(initParams).getInject(
+				RegionClient.class).into(UnivMobileServlet.class);
+
+		try {
+
+			regions.getRegions();
+
+		} catch (final IOException e) {
+			throw new ServletException(e);
+		}
+
 		super.init( //
 				new HomeController(), //
-				new AboutController() //
+				new AboutController(regions) //
 		);
+	}
+
+	private Map<String, String> loadInitParams() {
+
+		final Map<String, String> params = new HashMap<String, String>();
+
+		final ServletConfig servletConfig = getServletConfig();
+
+		for (final Enumeration<?> e = servletConfig.getInitParameterNames(); e
+				.hasMoreElements();) {
+
+			final String name = (String) e.nextElement();
+
+			final String value = servletConfig.getInitParameter(name);
+
+			params.put(name, value);
+		}
+
+		return params;
 	}
 }
